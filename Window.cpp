8778c480c08384466a5750dac1367ecd7290854e
Stephanie Jones <SDJones6@student.fullsail.edu>
2024-12-05 22:54:51 -0600

    start of event on click

--- Window.cpp
+++ Window.cpp
@@ -1,21 +1,21 @@
 #include "Window.h"
-//wxBEGIN_EVENT_TABLE(Window, wxFrame)
-//EVT_BUTTON(WX_ID_ANY, Window::OnButtonClicked)
-//wxEND_EVENT_TABLE()
+wxBEGIN_EVENT_TABLE(Window, wxFrame)
+	EVT_BUTTON(ID_BUTTON, Window::OnButtonClicked)
+wxEND_EVENT_TABLE()
 Window::Window ()
 	: wxFrame(nullptr, wxID_ANY, "Calculator", wxPoint(200, 200), wxSize(600, 600))
 {
 	//numbers
-	wxButton* button1 = new wxButton(this, wxID_ANY, "1", wxPoint(100, 10), wxSize (100,100));
-	wxButton* button2 = new wxButton(this, wxID_ANY, "2", wxPoint(120, 10), wxSize(100,100));
-	wxButton* button3 = new wxButton(this, wxID_ANY, "3", wxPoint(130, 10), wxSize(100, 100));
-	wxButton* button4 = new wxButton(this, wxID_ANY, "4", wxPoint(140, 10), wxSize(100, 100));
-	wxButton* button5 = new wxButton(this, wxID_ANY, "5", wxPoint(150, 10), wxSize(100, 100));
-	wxButton* button6 = new wxButton(this, wxID_ANY, "6", wxPoint(160, 10), wxSize(100, 100));
-	wxButton* button7 = new wxButton(this, wxID_ANY, "7", wxPoint(170, 10), wxSize(100, 100));
-	wxButton* button8 = new wxButton(this, wxID_ANY, "8", wxPoint(180, 10), wxSize(100, 100));
-	wxButton* button9 = new wxButton(this, wxID_ANY, "9", wxPoint(190, 10), wxSize(100, 100));
-	wxButton* button0 = new wxButton(this, wxID_ANY, "0", wxPoint(200, 10), wxSize(100, 100));
+	wxButton* button1 = new wxButton(this, ID_ONE, "1", wxPoint(100, 10), wxSize (100,100));
+	wxButton* button2 = new wxButton(this, ID_TWO, "2", wxPoint(120, 10), wxSize(100,100));
+	wxButton* button3 = new wxButton(this, ID_THREE, "3", wxPoint(130, 10), wxSize(100, 100));
+	wxButton* button4 = new wxButton(this, ID_FOUR, "4", wxPoint(140, 10), wxSize(100, 100));
+	wxButton* button5 = new wxButton(this, ID_FIVE, "5", wxPoint(150, 10), wxSize(100, 100));
+	wxButton* button6 = new wxButton(this, ID_SIX, "6", wxPoint(160, 10), wxSize(100, 100));
+	wxButton* button7 = new wxButton(this, ID_SEVEN, "7", wxPoint(170, 10), wxSize(100, 100));
+	wxButton* button8 = new wxButton(this, ID_EIGHT, "8", wxPoint(180, 10), wxSize(100, 100));
+	wxButton* button9 = new wxButton(this, ID_NINE, "9", wxPoint(190, 10), wxSize(100, 100));
+	wxButton* button0 = new wxButton(this, ID_ZERO, "0", wxPoint(200, 10), wxSize(100, 100));
 
 	//Unary Operator
 	wxButton* buttonSin = new wxButton(this, wxID_ANY, "Sin", wxPoint(120, 10), wxSize(100, 100));
@@ -41,4 +41,30 @@ Window::Window ()
 	
 }
 
-
+void Window::OnButtonClicked(wxCommandEvent& event)
+{
+	//get that lable
+	wxString label = event.GetString();
+	//numbers
+	if (label.IsNumber()) { 
+		textBox->AppendText(label); 
+	}
+	//clear
+	else if (label == "Clear") {
+		textBox->Clear();
+	}
+	else if (label == "=") {
+		wxString expression = textBox->GetValue();
+		textBox->Clear();
+	}
+	else if (label == "Back") {
+		wxString currentText = textBox->GetValue();
+		if (!currentText.IsEmpty()) {
+			currentText.RemoveLast();
+			textBox->SetValue(currentText);
+		}
+	}
+	else {
+		textBox->AppendText(label);
+	}
+}
--- Window.h
+++ Window.h
@@ -7,12 +7,27 @@ class Window : public wxFrame
 private:
 	//wxButton* button1; // = nullptr;
 	//wxButton* button2; // = nullptr;
-	//wxTextCtrl* textBox; // = nullptr;
+	wxTextCtrl* textBox; // = nullptr;
 	
-	//void OnButtonClicked(wxCommandEvent& event);
+	void OnButtonClicked(wxCommandEvent& event);
 
 public:
 	Window();
+	enum 
+	{
+		ID_BUTTON,
+		
+		ID_ONE,
+		ID_TWO,
+		ID_THREE,
+		ID_FOUR,
+		ID_FIVE,
+		ID_SIX,
+		ID_SEVEN,
+		ID_EIGHT,
+		ID_NINE,
+		ID_ZERO,
+	};
 	wxDECLARE_EVENT_TABLE();
 };
 
