0be078d336eac60945f5fe39764ccbc330c58730
Stephanie Jones <SDJones6@student.fullsail.edu>
2024-12-06 10:38:51 -0600

    put back the text editors

--- Window.cpp
+++ Window.cpp
@@ -10,13 +10,26 @@ EVT_BUTTON(ID_SEVEN, Window::OnButtonClicked)
 EVT_BUTTON(ID_EIGHT, Window::OnButtonClicked)
 EVT_BUTTON(ID_NINE, Window::OnButtonClicked)
 EVT_BUTTON(ID_ZERO, Window::OnButtonClicked)
+EVT_BUTTON(ID_PLUS, Window::OnButtonClicked)
+EVT_BUTTON(ID_MINUS, Window::OnButtonClicked)
+EVT_BUTTON(ID_MULTIPLY, Window::OnButtonClicked)
+EVT_BUTTON(ID_DIVIDE, Window::OnButtonClicked)
+EVT_BUTTON(ID_MODULO, Window::OnButtonClicked)
+EVT_BUTTON(ID_DECIMAL, Window::OnButtonClicked)
+EVT_BUTTON(ID_NEGATIVE, Window::OnButtonClicked)
+EVT_BUTTON(ID_EQUAL, Window::OnButtonClicked)
+EVT_BUTTON(ID_CLEAR, Window::OnButtonClicked)
+EVT_BUTTON(ID_BACKSPACE, Window::OnButtonClicked)
+EVT_BUTTON(ID_SIN, Window::OnButtonClicked)
+EVT_BUTTON(ID_COS, Window::OnButtonClicked)
+EVT_BUTTON(ID_TAN, Window::OnButtonClicked)
 wxEND_EVENT_TABLE()
 Window::Window ()
 	: wxFrame(nullptr, wxID_ANY, "Calculator", wxPoint(200, 200), wxSize(600, 600))
 {
 	//numbers
-	wxButton* button1 = new wxButton(this, ID_ONE, "1", wxPoint(0, 100), wxSize (100,100));
-	wxButton* button2 = new wxButton(this, ID_TWO, "2", wxPoint(100, 100), wxSize(100,100));
+	button1 = new wxButton(this, ID_ONE, "1", wxPoint(0, 100), wxSize (100,100));
+	button2 = new wxButton(this, ID_TWO, "2", wxPoint(100, 100), wxSize(100,100));
 	wxButton* button3 = new wxButton(this, ID_THREE, "3", wxPoint(200, 100), wxSize(100, 100));
 	wxButton* button4 = new wxButton(this, ID_FOUR, "4", wxPoint(0, 200), wxSize(100, 100));
 	wxButton* button5 = new wxButton(this, ID_FIVE, "5", wxPoint(100, 200), wxSize(100, 100));
@@ -27,56 +40,62 @@ Window::Window ()
 	wxButton* button0 = new wxButton(this, ID_ZERO, "0", wxPoint(0, 400), wxSize(100, 100));
 
 	//Unary Operator
-	wxButton* buttonSin = new wxButton(this, wxID_ANY, "Sin", wxPoint(120, 10), wxSize(100, 100));
-	wxButton* buttonCos = new wxButton(this, wxID_ANY, "Cos", wxPoint(120, 10), wxSize(100, 100));
-	wxButton* buttonTan = new wxButton(this, wxID_ANY, "Tan", wxPoint(120, 10), wxSize(100, 100));
+	wxButton* buttonSin = new wxButton(this, ID_SIN, "Sin", wxPoint(0, 0), wxSize(100, 100));
+	wxButton* buttonCos = new wxButton(this, ID_COS, "Cos", wxPoint(100, 100), wxSize(100, 100));
+	wxButton* buttonTan = new wxButton(this, ID_TAN, "Tan", wxPoint(200, 200), wxSize(100, 100));
 
 	//Binary Operator
-	wxButton* buttonPlus = new wxButton(this, wxID_ANY, "+", wxPoint(120, 10), wxSize(100, 100));
-	wxButton* buttonMinus = new wxButton(this, wxID_ANY, "-", wxPoint(120, 10), wxSize(100, 100));
-	wxButton* buttonMultiply = new wxButton(this, wxID_ANY, "*", wxPoint(120, 10), wxSize(100, 100));
-	wxButton* buttonDivide = new wxButton(this, wxID_ANY, "/", wxPoint(120, 10), wxSize(100, 100));
-	wxButton* buttonModulo = new wxButton(this, wxID_ANY, "%", wxPoint(120, 10), wxSize(100, 100));
+	wxButton* buttonPlus = new wxButton(this, ID_PLUS, "+", wxPoint(120, 10), wxSize(100, 100));
+	wxButton* buttonMinus = new wxButton(this, ID_MINUS, "-", wxPoint(120, 10), wxSize(100, 100));
+	wxButton* buttonMultiply = new wxButton(this, ID_MULTIPLY, "*", wxPoint(120, 10), wxSize(100, 100));
+	wxButton* buttonDivide = new wxButton(this, ID_DIVIDE, "/", wxPoint(120, 10), wxSize(100, 100));
+	wxButton* buttonModulo = new wxButton(this, ID_MODULO, "%", wxPoint(120, 10), wxSize(100, 100));
 
 	//Others
-	wxButton* buttonEqual = new wxButton(this, wxID_ANY, "=", wxPoint(300, 0), wxSize(100, 100));
-	wxButton* buttonClear = new wxButton(this, wxID_ANY, "Clear", wxPoint(300, 100), wxSize(100, 100));
-	wxButton* buttonDelete = new wxButton(this, wxID_ANY, "Back", wxPoint(300, 200), wxSize(100, 100));
-	wxButton* buttonDecimal = new wxButton(this, wxID_ANY, ".", wxPoint(300, 300), wxSize(100, 100));
-	wxButton* buttonNegative = new wxButton(this, wxID_ANY, "Neg", wxPoint(300, 400), wxSize(100, 100));
+	wxButton* buttonEqual = new wxButton(this, ID_EQUAL, "=", wxPoint(300, 0), wxSize(100, 100));
+	wxButton* buttonClear = new wxButton(this, ID_CLEAR, "Clear", wxPoint(300, 100), wxSize(100, 100));
+	wxButton* buttonBack = new wxButton(this, ID_BACKSPACE, "Back", wxPoint(300, 200), wxSize(100, 100));
+	wxButton* buttonDecimal = new wxButton(this, ID_DECIMAL, ".", wxPoint(300, 300), wxSize(100, 100));
+	wxButton* buttonNegative = new wxButton(this, ID_NEGATIVE, "Negative", wxPoint(300, 400), wxSize(100, 100));
 
 	//Text Control
-	wxTextCtrl* textBox = new wxTextCtrl(this, wxID_ANY, "", wxPoint(0, 0), wxSize(300, 100));
+	textBox = new wxTextCtrl(this, wxID_ANY, "", wxPoint(0, 0), wxSize(300, 100));
 	
 }
 
 void Window::OnButtonClicked(wxCommandEvent& event)
 {
-	//get that lable
 	wxString label = event.GetString();
-	//numbers
-	if (label.IsNumber()) { 
-		textBox->AppendText(label); 
-	}
-	//clear
-	else if (label == "Clear") {
-		textBox->Clear();
-	}
-	//Equals
-	else if (label == "=") {
-		wxString expression = textBox->GetValue();
-		textBox->Clear();
-	}
-	//Back
-	else if (label == "Back") {
-		wxString currentText = textBox->GetValue();
-		if (!currentText.IsEmpty()) {
-			currentText.RemoveLast();
+	wxString currentText = textBox->GetValue();
+	
+	//number
+		if (label.IsNumber()) {
+			wxString currentText = textBox->GetValue();
+			currentText += label;
 			textBox->SetValue(currentText);
-			//we need to add a catch here 
 		}
-	}
-	else {
-		textBox->AppendText(label);
-	}
+		//clear
+		else if (label == "Clear") {
+			textBox->Clear();
+		}
+
+		//back
+		else if (label == "Back") {
+			if (!currentText.IsEmpty()) {
+				currentText.Remove(currentText.Len() - 1);
+				textBox->SetValue(currentText);
+			}
+		}
+
+		//equal
+		else if (label == "=") {
+			if (currentText.IsEmpty()) {
+				throw std::invalid_argument("Empty");
+				return;
+			}
+			double result = 0;
+			bool validExpression = true;
+		}
 }
+
+ 
--- Window.h
+++ Window.h
@@ -5,29 +5,44 @@
 class Window : public wxFrame
 {
 private:
-	//wxButton* button1; // = nullptr;
-	//wxButton* button2; // = nullptr;
+	
 	wxTextCtrl* textBox; // = nullptr;
 	
-	void OnButtonClicked(wxCommandEvent& event);
+	
 
 public:
 	Window();
 	enum 
 	{
 		ID_BUTTON,
-		
-		ID_ONE,
-		ID_TWO,
-		ID_THREE,
-		ID_FOUR,
-		ID_FIVE,
-		ID_SIX,
-		ID_SEVEN,
-		ID_EIGHT,
-		ID_NINE,
-		ID_ZERO,
+		ID_TEXT,
+		ID_ONE = 1,
+		ID_TWO = 2,
+		ID_THREE = 3,
+		ID_FOUR = 4,
+		ID_FIVE = 5,
+		ID_SIX = 6,
+		ID_SEVEN = 7,
+		ID_EIGHT = 8,
+		ID_NINE = 9,
+		ID_ZERO = 0,
+		ID_PLUS,
+		ID_MINUS,
+		ID_MULTIPLY,
+		ID_DIVIDE,
+		ID_MODULO,
+		ID_DECIMAL,
+		ID_NEGATIVE,
+		ID_EQUAL,
+		ID_CLEAR,
+		ID_BACKSPACE,
+		ID_SIN,
+		ID_COS,
+		ID_TAN,
 	};
+	wxButton* button1 = nullptr;
+	wxButton* button2 = nullptr;
+	void OnButtonClicked(wxCommandEvent& event);
 	wxDECLARE_EVENT_TABLE();
 };
 
