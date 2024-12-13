255133d3a71c635d31de02f607653426b356b2e1
Stephanie Jones <SDJones6@student.fullsail.edu>
2024-12-12 22:25:27 -0600

    static buttons

--- ButtonFactory.h
+++ ButtonFactory.h
@@ -1,5 +1,14 @@
 #pragma once
+#include "wx/wx.h"
 class ButtonFactory
 {
+	static wxButton* CreateGenericButton;
+
+	static wxButton* CreateNumberButton;
+	static wxButton* CreateOperatorButton;
+	static wxButton* CreateUrnaryButton;
+	static wxButton* CreateOtherButton;
+	
+
 };
 
