fc1be789bdcb9b670940a02c831735b8de66ce22
Stephanie Jones <SDJones6@student.fullsail.edu>
2024-12-14 23:54:39 -0600

    lab 3 part 1

--- ButtonFactory.cpp
+++ ButtonFactory.cpp
@@ -1,8 +1,13 @@
 #include "ButtonFactory.h"
-
+//extra
 wxButton* ButtonFactory::CreateButton()
 {
 	return nullptr;
+}
+
+wxButton* ButtonFactory::CreateNumberButton(wxWindow* parent, int id, const wxString& label, const wxPoint& pos, const wxSize& size)
+{
+	return new wxButton(parent, id, label, pos, size);
 }
 
 wxButton* ButtonFactory::CreateAddButton(wxWindow* parent, int id, const wxString& label, const wxPoint& pos, const wxSize& size)
@@ -12,6 +17,36 @@ wxButton* ButtonFactory::CreateAddButton(wxWindow* parent, int id, const wxStrin
 }
 
 wxButton* ButtonFactory::CreateOperatorButton(wxWindow* parent, int id, const wxString& label, const wxPoint& pos, const wxSize& size)
+{
+	return new wxButton(parent, id, label, pos, size);
+}
+
+wxButton* ButtonFactory::CreateUnaryButton(wxWindow* parent, int id, const wxString& label, const wxPoint& pos, const wxSize& size)
+{
+	return new wxButton(parent, id, label, pos, size);
+}
+
+wxButton* ButtonFactory::CreateEqualButton(wxWindow* parent, int id, const wxString& label, const wxPoint& pos, const wxSize& size)
+{
+	return new wxButton(parent, id, label, pos, size);
+}
+
+wxButton* ButtonFactory::CreateClearButton(wxWindow* parent, int id, const wxString& label, const wxPoint& pos, const wxSize& size)
+{
+	return new wxButton(parent, id, label, pos, size);
+}
+
+wxButton* ButtonFactory::CreateBackButton(wxWindow* parent, int id, const wxString& label, const wxPoint& pos, const wxSize& size)
+{
+	return new wxButton(parent, id, label, pos, size);
+}
+
+wxButton* ButtonFactory::CreateDecimalButton(wxWindow* parent, int id, const wxString& label, const wxPoint& pos, const wxSize& size)
+{
+	return new wxButton(parent, id, label, pos, size);
+}
+
+wxButton* ButtonFactory::CreateNegativeButton(wxWindow* parent, int id, const wxString& label, const wxPoint& pos, const wxSize& size)
 {
 	return new wxButton(parent, id, label, pos, size);
 }
--- ButtonFactory.h
+++ ButtonFactory.h
@@ -6,8 +6,16 @@ public:
 	//Basic
 	static wxButton* CreateButton();
 	//Specific
+	static wxButton* CreateNumberButton(wxWindow* parent, int id, const wxString& label, const wxPoint& pos, const wxSize& size);
+	//in case everything needs to be seperate
 	static wxButton* CreateAddButton(wxWindow* parent, int id, const wxString& label, const wxPoint& pos, const wxSize& size);
 	static wxButton* CreateOperatorButton(wxWindow* parent, int id, const wxString& label, const wxPoint& pos, const wxSize& size);
+	static wxButton* CreateUnaryButton(wxWindow* parent, int id, const wxString& label, const wxPoint& pos, const wxSize& size);
+	static wxButton* CreateEqualButton(wxWindow* parent, int id, const wxString& label, const wxPoint& pos, const wxSize& size);
+	static wxButton* CreateClearButton(wxWindow* parent, int id, const wxString& label, const wxPoint& pos, const wxSize& size);
+	static wxButton* CreateBackButton(wxWindow* parent, int id, const wxString& label, const wxPoint& pos, const wxSize& size);
+	static wxButton* CreateDecimalButton(wxWindow* parent, int id, const wxString& label, const wxPoint& pos, const wxSize& size);
+	static wxButton* CreateNegativeButton(wxWindow* parent, int id, const wxString& label, const wxPoint& pos, const wxSize& size);
 	
 
 };
